a038ec110b24cc9e1c275d50a551514f7795dfc7
ndikrp <andhikarizky36@gmail.com>
2025-10-29 00:37:30 +0700

    fix: index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import "dotenv/config";
 import app from "./app.js";
 
-const PORT = process.env.PORT || 9000;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, "0.0.0.0", () => {
 	console.log("----------------------------------------------");
-	console.log(`Server is running on http://localhost:${PORT}`);
+	console.log(`Server is running on http://0.0.0.0:${PORT}`);
 	console.log("----------------------------------------------");
 });
